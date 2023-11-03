d8eaa3625ecccbe8b79ebaee1f1b7ecfc572f7db
git-avilabs <mail.avinashsah@gmail.com>
2023-11-03 09:42:05 +0530

    site added

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.avilabs.net",
   markdown: {
     remarkPlugins: [remarkReadingTime],
     shikiConfig: {
