1bd1fc4dff6c44adf07455a500d76a74c4437f3f
git-avilabs <mail.avinashsah@gmail.com>
2023-11-03 09:38:56 +0530

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,22 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     remarkPlugins: [remarkReadingTime],
-    shikiConfig: { theme: "css-variables", wrap: true },
+    shikiConfig: {
+      theme: "css-variables",
+      wrap: true,
+    },
   },
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
     mdx(),
+    sitemap(),
   ],
 });
